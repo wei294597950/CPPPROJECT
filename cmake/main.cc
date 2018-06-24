6e3a3d0cd9770a57a81e3d63cd47c58170dceaec
xuwei <294597950@qq.com>
2018-06-24 13:15:41 +0800

    cmake in windows

--- cmake/main.cc
+++ cmake/main.cc
@@ -11,13 +11,16 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3){
+    /*if (argc < 3){
         printf("Usage: %s base exponent \n", argv[0]);
         return 1;
     }
     double base = atof(argv[1]);
-    int exponent = atoi(argv[2]);
+    int exponent = atoi(argv[2]);*/
+	double base = 2.0;
+	int exponent = 3;
     double result = power(base, exponent);
     printf("%g ^ %d is %g\n", base, exponent, result);
+	getchar();
     return 0;
 }
